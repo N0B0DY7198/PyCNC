09da29e6873068b911482f3fc44aa2cbc687e5ad
N0B0DY7198 <95550822+N0B0DY7198@users.noreply.github.com>
2022-06-17 10:54:51 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 clear
+chmod +x CNC
 cat readme.txt
 echo Starting in 10 sec...
 sleep 10
-apt install python3-pip
-pip3 install pystyle
-pip3 install threading
 echo Any missing modules install them with 'pip3 install [module]'
 echo connect like this: IP: [server ip] PORT: Your choice TYPE: RAW
-sudo python3 main.py
+screen ./CNC
