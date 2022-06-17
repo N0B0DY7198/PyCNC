7149b70cb1aa24524b2c61713c54de83f734f7b0
N0B0DY7198 <95550822+N0B0DY7198@users.noreply.github.com>
2022-06-16 21:26:45 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ apt install python3-pip
 pip3 install pystyle
 pip3 install threading
 echo Any missing modules install them with 'pip3 install [module]'
-connect like this: IP: [server ip] PORT: Your choice TYPE: RAW
+echo connect like this: IP: [server ip] PORT: Your choice TYPE: RAW
 sudo python3 main.py
